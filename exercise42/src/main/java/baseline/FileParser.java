52129e992eb0a964feb6b086b8316bceac63be70
peteperri <peteperri4@gmail.com>
2021-10-11 22:33:58 -0400

    ex42 pseudocode and UML

---
+++ exercise42/src/main/java/baseline/FileParser.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Peter Perri
+ */
+package baseline;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+
+public class FileParser {
+
+   public FileReader getFile(String fileName) throws FileNotFoundException {
+       //return a new FileReader object that takes the fileName as a parameter
+   }
+
+    public List<List<String>> convertCSVtoStrings(FileReader csv) throws IOException {
+       //make an ArrayList of ArrayLists of Strings
+        //parse and split up the file while adding each individual element to the List
+    }
+
+
+
+}
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Peter Perri
+ */
+package baseline;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Solution42 {
+    public static void main(String[] args) throws IOException {
+        //create instances of a file parser and of Solution42
+        //make a new FileReader and set it to the result FileParser's getFile function
+        //make an ArrayList of ArrayLists of Strings called data
+        //pass data to the Parser's function that will convert the CSV into an ArrayList of an ArrayList of Strings
+        //pass the ArrayList to the output creator function
+    }
+    private void createOutput(List<List<String>> data){
+        //formatted print statement to print every element of data
+        }
+    }
+}
